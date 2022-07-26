b8a688918c58db0d9b55f8b5a9531f666ae82601
Palink <58388698+doomclouds@users.noreply.github.com>
2022-07-26 08:41:18 +0800

    添加项目文件。

---
+++ Palink.Wpf/ControlExt/ControlExtensions.cs
@@ -0,0 +1,47 @@
+﻿using System;
+using System.IO;
+using System.Windows;
+using System.Windows.Media;
+using System.Windows.Media.Imaging;
+
+namespace Palink.Wpf.ControlExt;
+
+public static class ControlExtensions
+{
+    public static void SaveControlContent(this FrameworkElement element, string fileName)
+    {
+        var bounds = VisualTreeHelper.GetDescendantBounds(element);
+        const double dpi = 96d;
+
+        var rtb = new RenderTargetBitmap((int)bounds.Width,
+            (int)bounds.Height, dpi, dpi, PixelFormats.Default);
+
+        var dv = new DrawingVisual();
+        using (var dc = dv.RenderOpen())
+        {
+            var vb = new VisualBrush(element);
+            dc.DrawRectangle(vb, null, new Rect(new Point(), bounds.Size));
+        }
+
+        rtb.Render(dv);
+
+        BitmapEncoder pngEncoder = new PngBitmapEncoder();
+        pngEncoder.Frames.Add(BitmapFrame.Create(rtb));
+
+        try
+        {
+            var ms = new MemoryStream();
+
+            pngEncoder.Save(ms);
+            ms.Close();
+
+            File.WriteAllBytes(fileName, ms.ToArray());
+        }
+        catch (Exception err)
+        {
+            MessageBox.Show(err.ToString(), "Error",
+                MessageBoxButton.OK,
+                MessageBoxImage.Error);
+        }
+    }
+}
---
+++ Palink.Wpf/WpfHelper.cs
@@ -0,0 +1,26 @@
+﻿using System.Windows.Forms;
+using Palink.Wpf.ScreenExt;
+
+namespace Palink.Wpf;
+
+public class WpfHelper
+{
+    public static (double scaleX, double scaleY, Screen current) GetDpiScale(
+        int index)
+    {
+        var screen = Screen.AllScreens[0];
+        screen.GetDpi(DpiType.Effective, out var xDpi, out var yDpi);
+        var scaleX = xDpi / 96.0;
+        var scaleY = yDpi / 96.0;
+        if (index >= Screen.AllScreens.Length)
+        {
+            return (scaleX, scaleY, screen);
+        }
+
+        screen = Screen.AllScreens[index];
+        screen.GetDpi(DpiType.Effective, out xDpi, out yDpi);
+        scaleX = xDpi / 96.0;
+        scaleY = yDpi / 96.0;
+        return (scaleX, scaleY, screen);
+    }
+}
